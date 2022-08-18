351cb6e2aa5e55f5e020a165f0900bd192c4934f
terry <terry.cadd@gmail.com>
2022-08-19 00:28:51 +0100

    writing routine to sweep board and see if a row has been completed - increasing score

--- tetris.lisp
+++ tetris.lisp
@@ -14,6 +14,7 @@
 
 
 
+
 ;; go through tetris.scm originally written in guile to convert to common
 
 
@@ -613,6 +614,271 @@
 
 ;; -------------------  -------------------------------------
 
+
+(defun margin()
+  (format t "                "))
+
+
+;; iterate over board looking for x y
+(defun scan-board( x-y board)
+  (cond
+   ((null board) nil)
+   ((equalp x-y (car board)) x-y)
+   (t (scan-board x-y (cdr board)))))
+
+
+(defun newline()
+  (format t "~%"))
+
+
+
+;; if square is occupied then put a # otherwise a dot .
+(defun show-board-squares( x y board)
+  (cond
+   ((> x 11) (newline)(margin)(show-board-squares 0 (- y 1) board))
+   ((< y 0) (newline))
+   (t    
+    (cond
+     ((scan-board (list x y) board)
+      (format t " X "))
+     (t
+      (format t " . ")))
+    (show-board-squares (+ x 1) y board))))
+
+
+(defun display(msg)
+  (format t "~a" msg))
+
+
+
+
+(defun show-board(board)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (margin)(display " T E T R I Z   v  1 . 0") (newline)
+  (newline)
+  (newline)
+  (margin)
+  (display "_ _ _ _ _ _ _ _ _ _ _ _")
+  (newline)
+  (newline)
+  (margin)
+  (show-board-squares 0 21 board)
+  (newline)
+  (margin)
+  (display "_ _ _ _ _ _ _ _ _ _ _ _")
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+  (newline)
+)
+
+
+
+
+
+;; iterate over board - see if any piece on board conflicts with piece
+(defun any-conflicts?(piece board)
+  (cond
+   ((null board) nil)
+   ((conflict-p piece (first board)) (first board))
+   (t (any-conflicts? piece (cdr board)))))
+
+
+
+;;; --------------------------- 712 -------------------------------
+
+
+                ;; _ _ _ _ _ _ _ _ _ _ _ _
+
+                ;;  T E T R I Z   v  1 . 0
+
+                ;; _ _ _ _ _ _ _ _ _ _ _ _
+
+                ;;
+                ;;(0,21)                           (11,21)
+                ;;  X  X  X  X  X  X  X  X  X  X  X  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  X  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  X  X  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  X  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  .  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  X  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  X  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  X  .  .  .  X 
+                ;;  X  .  .  .  .  .  .  X  .  .  .  X 
+                ;;  X  X  X  X  X  X  X  X  X  X  X  X 
+                ;; (0,0)                            (11,0)
+
+                ;;  tetris board is 10 wide  x 20 high
+                ;; _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+
+;;
+;; list of squares '( (1 1)(4 2)(3 4).... )
+;; 30 arbitrary number - as long as its bigger than height of the tetris table
+;;
+(defun eliminate-complete-rows(board)
+  (let ((arr (make-array 30)))
+    (loop (dolist xy board t)
+	  (let ((x (first xy))
+		(y (second xy)))
+	    (if (and (>= x 0) (< x 10
+	    (incf (aref arr y))))
+    ;; for any entry in arr that equals 10 
+  )
+
+
+(defun combine-piece-and-board(piece board)
+  (let ((squares (realise piece)))
+    (append squares board)))
+
+
+
+
+;; accept - move piece down continuously until stops
+(defun game-ask(piece board)
+  (newline)
+  (show-board (combine-piece-and-board piece board))
+  (newline)
+  (display "Your move :> enter S A D N or M ")	      
+  (newline)  
+  (display "south!(s) : left(a) : right(d) : rot-right(n) : rot-left(m) : fix-in-place(f) : quit(q)> ")
+  (let ((response (read)))
+    (newline)
+    (display "response =[")
+    (display response)
+    (display "]")
+    (cond
+     ((or (equalp response 's))
+      (display "understood as s for south move")
+      (if (any-conflicts? (down piece) board)
+	  (progn
+	    (newline)(display "cannot go SOUTH - conflicts detected")
+	    (game-ask piece board))
+	  (game-ask (down piece) board)))
+     
+     ((or (equalp response 'a))
+      (display "understood as a for LEFT")
+      (if (any-conflicts? (left piece) board)
+	  (progn
+	    (newline)(display "cannot go LEFT any more - conflicts detected")
+	    (game-ask piece board))
+	  (game-ask (left piece) board)))     
+      
+     ((or (equalp response 'd))
+      (display "understood as d for RIGHT")
+      (if (any-conflicts? (right piece) board)
+	  (progn
+	    (newline)(display "cannot go RIGHT any more - conflicts detected")
+	    (game-ask piece board))
+	  (game-ask (right piece) board)))
+     
+     ((or (equalp response 'n))
+      (display "understood as n for ROTATE rIGHT")
+      (if (any-conflicts? (rotate-right piece) board)
+	  (progn
+	    (newline)(display "cannot rotate RIGHT  - conflicts detected")
+	    (game-ask piece board))
+	  (game-ask (rotate-right piece) board)))
+           
+     ((or (equalp response 'm))
+      (display "understood as m for ROTATE LEFT")      
+      (if (any-conflicts? (rotate-left piece) board)
+	  (progn
+	    (newline)(display "cannot rotate LEFT - conflicts detected")	    
+	    (game-ask piece board))
+	  (game-ask (rotate-left piece) board)))
+
+     ((or (equalp response 'f))
+      (display "understood as f for COMPLETED MOVE")      
+      (if (any-conflicts? (down piece) board)
+	  (progn
+	    (newline)(display "we are done with this piece !")
+	    (game-loop (eliminate-completed-rows (combine-piece-and-board piece board))))
+	  (game-ask (rotate-left piece) board)))
+     
+
+     ((or (equalp response 'q))
+      (display "understood as q for QUIT")
+      (error "quit from game - q - "))     
+                 
+     (t (newline)
+	   (display "...not understood ...")
+	   (display response)
+	   (newline)
+	   (game-ask piece board)))))
+
+
+
+(defvar *tetris-piece-constructors*
+  (list #'make-flat
+	#'make-box
+	#'make-right-bend
+	#'make-left-bend
+	#'make-elbow
+	#'make-junction))
+
+
+     
+(defun game-loop(board)
+  (let ((piece (funcall (one-of *tetris-piece-constructors*)
+			   5 20)))
+    (cond
+     ((any-conflicts? piece board)
+      (newline)
+      (newline)
+      (display "G a m e ... O v e r"))
+     (t
+      (game-ask piece board)))))
+
+
+(defun game()
+  (let ((board '((0 0)(1 0) (2 0)(3 0)(4 0) (5 0)(6 0)(7 0)(8 0)(9 0)(10 0)(11 0)
+		 (0 1)(0 2) (0 3)(0 4)(0 5) (0 6)(0 7)(0 8)(0 9)(0 10)(0 11)(0 12)(0 13)(0 14)(0 15)(0 16)(0 17)(0 18)(0 19)(0 20)(0 21)
+		 (11 1)(11 2) (11 3)(11 4)(11 5) (11 6)(11 7)(11 8)(11 9)(11 10)(11 11)(11 12)(11 13)(11 14)(11 15)(11 16)(11 17)(11 18)(11 19)(11 20)(11 21)
+		 (0 21)(1 21) (2 21)(3 21)(4 21) (5 21)(6 21)(7 21)(8 21)(9 21)(10 21)(11 21))))
+    (game-loop board)))
+		 
+		 
+(game)
 
 
 
